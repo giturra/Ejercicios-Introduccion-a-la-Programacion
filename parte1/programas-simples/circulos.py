f5081e4bcf8573346855a2a46788c0f1b0d30db1
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2021-05-04 15:29:46 -0400

    algunos ejercicios simples

---
+++ parte1/programas-simples/circulos.py
@@ -0,0 +1,5 @@
+radius = int(input("Ingrese el radio: "))
+perimeter = 2 * radius * 3.14
+area = radius ** 2 * 3.14
+print("Perimetro:", round(perimeter, 1))
+print("Área:", round(area, 1))
---
+++ parte1/programas-simples/promedio.py
@@ -0,0 +1,6 @@
+n1 = int(input("Primera nota: "))
+n2 = int(input("Segunda nota: "))
+n3 = int(input("Tercera nota: "))
+n4 = int(input("Cuarta nota: "))
+mean = (n1 + n2 + n3 + n4) / 4
+print("El promedio es:", round(mean, 2))
